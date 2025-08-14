fa527ae619f4499684443ff2edcac38d04fcc93c
salah <salahalhimyari2023@gmail.com>
2025-08-15 00:10:20 +0300

    add config.php to project

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Configuration constants
+return [
+    'tax_rate' => 0.15, // 15% tax
+    'discount_percentage' => 0.10, // 10% discount
+    'database' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'dbname' => 'ministore'
+    ]
+];
